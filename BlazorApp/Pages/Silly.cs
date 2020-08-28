26bbb1e1285749dca0477930da682bb8fb62c998
Dave Padot <davepadot@users.noreply.github.com>
2020-08-28 15:21:06 -0400

    Add project files.

---
+++ BlazorApp/Pages/Silly.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp.Pages
+{
+    public partial class Silly
+    {
+        [Parameter] public string Name { get; set; }
+        protected override async Task OnInitializedAsync()
+        {
+            
+        }
+    }
+}
